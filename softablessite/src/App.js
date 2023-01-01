14198a5b706e77da644e1c21ecb0400052fa2dbc
Nicholas Mazza <nick@mazzafamily.ca>
2022-12-31 23:24:11 -0500

    Initialization of Site
    Startup of npx install and the installation of Tailwind CSS

---
+++ softablessite/src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className='grid grid-cols-1 bg-gray-100 font-serif'>
+
+      <div className='py-2'>
+        <p className='text-4xl text-center'>Softables</p>
+
+        <div className='text-center flex'>
+        <p className='text-color-black hover:text-sky-400 flex-auto'>Home</p>
+        <p className='text-color-black hover:text-sky-400 flex-auto'>Catalog</p>
+        <p className='text-color-black hover:text-sky-400 flex-auto'>Contact Us</p>
+        </div>
+
+      </div>
+
+      <div>
+        test 2
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
